d3c76736bf315961053cd906c5d84f00c423133a
siddhartha bordloi <sidbordoloi00@gmail.com>
2026-08-20 12:58:23 +0530

    Update server port for deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { execFile } = require("child_process");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const PYTHON = "C:\\Users\\Admin\\OneDrive\\AI-Video-Maker\\ai_env\\Scripts\\python.exe";
 
